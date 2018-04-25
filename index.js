9838ec20f49a661444fa0cd440dd46c9778ff3b9
malorie16 <malorie18@gmail.com>
2018-04-25 23:10:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+
+  for (var i = 0; i < musicians.length; i++) {
+    return array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+return array
+}
